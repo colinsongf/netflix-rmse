caf4be93768bf2201f0a7089d263feb3ce24c0bb
= <starmansix@gmail.com>
2015-06-24 15:44:28 -0500

    Fleshing out netflix methods

--- Netflix.py
+++ Netflix.py
@@ -2,18 +2,30 @@
 
 def netflix_read(r):
     """
+    Read a line from input file
     """
+    return r.readline()
 
 def netflix_print(s, w):
     """
+    Write a line to output
     """
+    w.write(str(s)+"\n")
 
 def netflix_rate(r, w):
     """
     """
+    while (True):
+        line = netflix_read(r)
+        if (line == ""):
+            return
+        
+        if (line[-1] != ":"):
+            netflix_print(netflix_predict(line), w)
+        else:
+            netflix_print(line, w)
 
 def netflix_predict(c):
     """
     """
-
-
+    return 1
