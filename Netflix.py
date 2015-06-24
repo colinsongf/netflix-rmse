3802157fc0e8d9c1a2bbdd279ee3b2e2748576c2
= <starmansix@gmail.com>
2015-06-24 15:25:29 -0500

    Adding skeleton for netflix methods

---
+++ Netflix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def netflix_read(r):
+    """
+    """
+
+def netflix_print(s, w):
+    """
+    """
+
+def netflix_rate(r, w):
+    """
+    """
+
+def netflix_predict(c):
+    """
+    """
+
+
