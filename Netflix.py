84a19af6672736b98e07293e6e2d68bc106b15fe
= <starmansix@gmail.com>
2015-06-24 17:12:58 -0500

    Added rmse method

--- Netflix.py
+++ Netflix.py
@@ -4,7 +4,7 @@ def netflix_read(r):
     """
     Read a line from input file
     """
-    return r.readline()
+    return r.readline()[ : -1]    # Do not read the newline character
 
 def netflix_print(s, w):
     """
@@ -17,6 +17,7 @@ def netflix_rate(r, w):
     """
     while (True):
         line = netflix_read(r)
+
         if (line == ""):
             return
         
@@ -26,6 +27,15 @@ def netflix_rate(r, w):
             netflix_print(line, w)
 
 def netflix_predict(c):
-    """
-    """
     return 1
+
+def rmse(a, p):
+    """
+    O(1) in space
+    O(n) in time
+    """
+    return sqrt(mean(square(subtract(a, p)))
+
+    #z = zip(a, p)
+    #v = sum([(x - y) ** 2 for x, y in z])
+    #return sqrt(v / len(a))
